e47a08e31e5d2fbc3b093dd556ccf4a69225bfea
Sam Collins <sam.collins@atomjuice.com>
2018-12-07 11:55:56 +0000

    Latest theme updates from upstream 2.7.0

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
 var zip = require('gulp-zip');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -24,9 +26,11 @@ var nodemonServerInit = function () {
     livereload.listen(1234);
 };
 
-gulp.task('build', ['css'], function (/* cb */) {
+gulp.task('build', ['css', 'js'], function (/* cb */) {
     return nodemonServerInit();
 });
+
+gulp.task('generate', ['css', 'js']);
 
 gulp.task('css', function () {
     var processors = [
@@ -38,19 +42,33 @@ gulp.task('css', function () {
     ];
 
     return gulp.src('assets/css/*.css')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(livereload());
+});
+
+gulp.task('js', function () {
+    var jsFilter = filter(['**/*.js'], {restore: true});
+
+    return gulp.src('assets/js/*.js')
+    .on('error', swallowError)
+    .pipe(sourcemaps.init())
+    .pipe(jsFilter)
+    .pipe(uglify())
+    .pipe(jsFilter.restore)
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('assets/built/'))
+    .pipe(livereload());
 });
 
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['css']);
 });
 
-gulp.task('zip', ['css'], function () {
+gulp.task('zip', ['css', 'js'], function () {
     var targetDir = 'dist/';
     var themeName = require('./package.json').name;
     var filename = themeName + '.zip';
@@ -60,8 +78,8 @@ gulp.task('zip', ['css'], function () {
         '!node_modules', '!node_modules/**',
         '!dist', '!dist/**'
     ])
-        .pipe(zip(filename))
-        .pipe(gulp.dest(targetDir));
+    .pipe(zip(filename))
+    .pipe(gulp.dest(targetDir));
 });
 
 gulp.task('default', ['build'], function () {
